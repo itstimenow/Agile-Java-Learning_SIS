b444dff33f191e2da4950b38a880349dfc83b529
itstimenow <mapeiworld@gmail.com>
2013-04-19 14:34:53 +0800

    Finish section 9 to 11 of lesson 5: Grading Students, Floating-Point Number, Testing Grades

--- sis/studentinfo/Student.java
+++ sis/studentinfo/Student.java
@@ -1,5 +1,7 @@
 package sis.studentinfo;
 
+import java.util.ArrayList;
+
 public class Student {
     static final int CREDITS_REQUIRED_FOR_FULL_TIME = 12;
     static final String IN_STATE = "CO";
@@ -7,6 +9,7 @@ public class Student {
     private String name;
     private int credits = 0;
     private String state = "";
+    private ArrayList<String> grades = new ArrayList<String>();
     
     public Student(String name) {
         this.name = name;
@@ -34,5 +37,28 @@ public class Student {
     
     boolean isInState() {
         return state.equals(Student.IN_STATE);
+    }
+    
+    void addGrade(String grade) {
+        grades.add(grade);
+    }
+    
+    double getGpa() {
+        if (grades.isEmpty())
+            return 0.0;
+        
+        double total = 0.0;
+        for (String grade : grades) {
+            if (grade.equals("A"))
+                total += 4;
+            else if (grade.equals("B"))
+                total += 3;
+            else if (grade.equals("C"))
+                total += 2;
+            else if (grade.equals("D"))
+                total += 1;
+        }
+        
+        return total / grades.size();
     }
 }
--- sis/studentinfo/StudentTest.java
+++ sis/studentinfo/StudentTest.java
@@ -3,6 +3,8 @@ package sis.studentinfo;
 import junit.framework.TestCase;
 
 public class StudentTest extends TestCase {
+    private static final double GRADE_TOLERANCE = 0.05;
+    
     public void testCreate() {
         final String firstStudentName = "Jane Doe";
         Student firstStudent = new Student(firstStudentName);
@@ -46,5 +48,20 @@ public class StudentTest extends TestCase {
         
         student.setState("co");
         assertTrue(student.isInState());
+    }
+    
+    public void testCalculateGpa() {
+        Student student = new Student("a");
+        assertEquals(0.0, student.getGpa(), GRADE_TOLERANCE);
+        student.addGrade("A");
+        assertEquals(4.0, student.getGpa(), GRADE_TOLERANCE);
+        student.addGrade("B");
+        assertEquals(3.5, student.getGpa(), GRADE_TOLERANCE);
+        student.addGrade("C");
+        assertEquals(3.0, student.getGpa(), GRADE_TOLERANCE);
+        student.addGrade("D");
+        assertEquals(2.5, student.getGpa(), GRADE_TOLERANCE);
+        student.addGrade("F");
+        assertEquals(2.0, student.getGpa(), GRADE_TOLERANCE);
     }
 }
