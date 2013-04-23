7fdaf7e9153c4112322c4e0db157c3efb9c06a67
itstimenow <mapeiworld@gmail.com>
2013-04-23 15:54:56 +0800

    Complete section 8 of lesson 6: Enhancing the Grade Enum

--- sis/studentinfo/Student.java
+++ sis/studentinfo/Student.java
@@ -3,7 +3,19 @@ package sis.studentinfo;
 import java.util.*;
 
 public class Student {
-    public enum Grade { A, B, C, D, F }
+    public enum Grade {
+        A(4), B(3), C(2), D(1), F(0);
+        
+        private int points;
+        
+        Grade(int points) {
+            this.points = points;
+        }
+        
+        int getPoints() {
+            return points;
+        }
+    }
     
     static final int CREDITS_REQUIRED_FOR_FULL_TIME = 12;
     static final String IN_STATE = "CO";
