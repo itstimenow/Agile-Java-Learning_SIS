6b00bd2cc6b793d017ed14208c7296d69c0ca629
itstimenow <mapeiworld@gmail.com>
2013-05-13 14:43:38 +0800

    Complete section 7 of lesson 10: Infinity

--- src/languageTest/MathmaticsTest.java
+++ src/languageTest/MathmaticsTest.java
@@ -12,5 +12,40 @@ public class MathmaticsTest extends TestCase {
         assertFalse(1.0 > Double.NaN);
         assertFalse(1.0 < Double.NaN);
         assertFalse(1.0 == Double.NaN);
+        
+        assertFalse(Float.NaN > 1.0);
+        assertFalse(Float.NaN < 1.0);
+        assertFalse(Float.NaN == 1.0);
+        assertFalse(1.0 > Float.NaN);
+        assertFalse(1.0 < Float.NaN);
+        assertFalse(1.0 == Float.NaN);
+    }
+    
+    public void testInfinity() {
+        final float tolerance = 0.5F;
+        final float x = 1F;
+        
+        assertEquals(Float.POSITIVE_INFINITY, Float.POSITIVE_INFINITY * 100, tolerance);
+        assertEquals(Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY * -1, tolerance);
+        
+        assertEquals(Float.POSITIVE_INFINITY, x / 0f, tolerance);
+        assertEquals(Float.NEGATIVE_INFINITY, x / -0f, tolerance);
+        assertTrue(Float.isNaN(x % 0f));
+        assertEquals(0f, x / Float.POSITIVE_INFINITY, tolerance);
+        assertEquals(-0f, x / Float.NEGATIVE_INFINITY, tolerance);
+        assertEquals(x, x % Float.POSITIVE_INFINITY, tolerance);
+        assertTrue(Float.isNaN(0f / 0f));
+        assertTrue(Float.isNaN(0f % 0f));
+        assertEquals(Float.POSITIVE_INFINITY, Float.POSITIVE_INFINITY / x, tolerance);
+        assertEquals(Float.NEGATIVE_INFINITY, Float.NEGATIVE_INFINITY / x, tolerance);
+        assertTrue(Float.isNaN(Float.POSITIVE_INFINITY % x));
+        assertTrue(Float.isNaN(Float.POSITIVE_INFINITY / Float.POSITIVE_INFINITY));
+        assertTrue(Float.isNaN(Float.POSITIVE_INFINITY % Float.POSITIVE_INFINITY));
+        assertTrue(Float.isNaN(Float.POSITIVE_INFINITY / Float.NEGATIVE_INFINITY));
+        assertTrue(Float.isNaN(Float.POSITIVE_INFINITY % Float.NEGATIVE_INFINITY));
+        assertTrue(Float.isNaN(Float.NEGATIVE_INFINITY / Float.POSITIVE_INFINITY));
+        assertTrue(Float.isNaN(Float.NEGATIVE_INFINITY % Float.POSITIVE_INFINITY));
+        assertTrue(Float.isNaN(Float.NEGATIVE_INFINITY / Float.NEGATIVE_INFINITY));
+        assertTrue(Float.isNaN(Float.NEGATIVE_INFINITY % Float.NEGATIVE_INFINITY));
     }
 }
