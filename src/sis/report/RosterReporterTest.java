5662e4d152e755a80dcf7fb0a57403e9ee0a3dc8
itstimenow <mapeiworld@gmail.com>
2013-05-25 21:06:02 +0800

    Complete section 4 of lesson 11: java.io.File

--- src/sis/report/RosterReporterTest.java
+++ src/sis/report/RosterReporterTest.java
@@ -24,17 +24,25 @@ public class RosterReporterTest extends TestCase {
     
     public void testFiledReport() throws IOException {
         final String filename = "testFiledReport.txt";
-        new RosterReporter(session).writeReport(filename);
         
-        StringBuffer buffer = new StringBuffer();
-        String line;
+        try {
+            delete(filename);
+            
+            new RosterReporter(session).writeReport(filename);
         
-        BufferedReader reader = new BufferedReader(new FileReader(filename));
-        while ((line = reader.readLine()) != null)
-            buffer.append(String.format(line + "%n"));
-        reader.close();
-        
-        assertReportContents(buffer.toString());
+            StringBuffer buffer = new StringBuffer();
+            String line;
+            
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            while ((line = reader.readLine()) != null)
+                buffer.append(String.format(line + "%n"));
+            reader.close();
+            
+            assertReportContents(buffer.toString());
+        }
+        finally {
+            delete(filename);
+        }        
     }
     
     private void assertReportContents(String rosterReport) {
@@ -44,5 +52,11 @@ public class RosterReporterTest extends TestCase {
                                    + RosterReporter.ROSTER_REPORT_FOOTER,
                                    2),
                      rosterReport);
+    }
+    
+    private void delete(String filename) {
+        File file = new File(filename);
+        if (file.exists())
+            assertTrue("unable to delete " + filename, file.delete());
     }
 }
